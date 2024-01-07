341a7b0e2c4d00e5cf3913d70eebe1434379227c
Prideland-Okoi <prideland.okoi@gmail.com>
2024-01-07 20:15:14 +0100

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+import pytest
+from flask import Flask
+
+from Jenkins-docker-k8 import app
+
+@pytest.fixture
+def client():
+    # Create a test client using Flask's test_client
+    with app.test_client() as client:
+        yield client
+
+def test_hello_endpoint(client):
+    # Test the / endpoint
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello, Jenkins Docker Kubernetes Project!' in response.data
